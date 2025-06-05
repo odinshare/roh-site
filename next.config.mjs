052f098616d497e3a483bf981128862c697d22c5
Kevin A. Mortimer <kevinmortimer56@gmail.com>
2025-06-04 23:07:11 -0400

    changes to build instructions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Force every page to be prerendered and exported
+  output: "export",
+};
 
 export default nextConfig;
